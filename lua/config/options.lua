b62fae5d427427df893d41eb694f91d3cfba0977
G.A.JAGUAR <dev@gajaguar.com>
2025-06-09 11:35:57 -0600

    chore(config): set pylsp as python linter

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,5 +2,6 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.g.python3_host_prog = "$HOME/venvs/nvim/bin/python"
 -- vim.g.clipboard = "unnamedplus"
+vim.g.lazyvim_python_lsp = "pylsp"
+vim.g.python3_host_prog = "$HOME/venvs/nvim/bin/python"
